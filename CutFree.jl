5f9bc07bad4fd5cf9b0c43a4c0f0f7a3be179b42
Walker Rickord <wrickord27@gmail.com>
2022-03-03 22:50:02 -0600

    Completed markdown file, fixed get_blocking_codes, completed subcodes, completed compiment

--- CutFree.jl
+++ CutFree.jl
@@ -16,23 +16,9 @@ IUB_CODES = Dict(
     "N" => ["A", "C", "G", "T"]
 )
 
-#= IUB_CODES Description
-    Dictionary consisting of keys and values (IUB codes)
-
-    Keys:
-        Input to call a value of the Dictionary
-    Values:
-        Output of the IUB codes used by CutFree
-=#
-
-# IUB_CODES Testing
-    # println(IUB_CODES["R"][2])
-# 
-
 function get_blocking_codes(code, names)
     blocking_codes = Vector()
 
-    # Constant Time: O(1)
     for (key, value) in IUB_CODES
         if filter(x -> x != code, value) == IUB_CODES[key] && !names
             push!(blocking_codes, value)
@@ -46,144 +32,87 @@ function get_blocking_codes(code, names)
     return blocking_codes
 end
 
-#= get_blocking_codes Description
-    Find codes that do not contain a code.
-
-    Example: 
-    If code is A, 
-        Then blocking codes are C,G,T,Y,K,S,B (all codes without A)
-    If names==TRUE, 
-        return the codes (character vector of bases);
-    otherwise return the code names.
-=#
-
-# get_blocking_codes Testing
-    # println(get_blocking_codes("A", false))
-# 
-
 function str_to_vector(str)
-    # Linear Time: O(n)?
-    return collect(str)
+    return split(str, "")
 end
 
-#= str_to_vector Description
-    Turn a string into a vector of single characters.
-
-    Example: 
-    If input str is ACGT, 
-    Then returned vector is ['A', 'C', 'G', 'T']
-=#
-
-# str_to_vector Testing
-    # println(str_to_vector("CGTACCCGG"))
-# 
-
 function make_oligo_block(oligo)
-    output = ""
     oligo_bases = str_to_vector(oligo)
-    bases = Dict("A" => false, "C" => false, "G" => false, "T" => false)
+    strings = Dict("A" => "", "C" => "", "G" => "", "T" => "")
 
-    # Linear Time: O(n)
+    i = 0
     for code in oligo_bases
-        # Constant Time: O(1)
-        for base in IUB_CODES[string(code)]
-            if (!bases[base])
-                bases[base] = true
+        for base in IUB_CODES[code]
+            for (key, value) in strings
+                if (key == base)
+                    strings[key] *= key
+                end
+            end
+        end
+
+        i += 1
+        for (key, value) in strings
+            if (sizeof(value) != i)
+                strings[key] *= "-"
             end
         end
     end
 
-    # Constant Time: O(1)
-    for (key, value) in bases
-        if value
-            output *= key * ": present \n"
-        else
-            output *= key * ": not present \n"
-        end
-    end
-
-    return output
+    return collect(values(strings))
 end
-
-#= make_oligo_block Description
-    creates a text block showing which bases are allowed
-    by the oligo. It returns four strings denoting the presence
-    of each base.
-=#
-
-# make_oligo_block Testing
-    # make_oligo_block("ACGC")
-# 
 
 function print_oligo_block(oligo)
-    println(oligo * "\n" * make_oligo_block(oligo))
+    return print(oligo * "\n" * make_oligo_block(oligo)[1] * "\n" * make_oligo_block(oligo)[2]
+    * "\n" * make_oligo_block(oligo)[3] * "\n" * make_oligo_block(oligo)[4])
 end
-
-#= print_oligo_block Description
-    prints the oligo and the output of make_oligo_block
-=#
-
-# print_oligo_block Testing
-    # print_oligo_block("ACGC")
-# 
-
-
 
 function xiny(x, y)
     return (x ∈ y)
 end
 
-#= xiny Description
-    returns a boolean value of whether or not x is found in y
-=#
-
-# xiny Testing
-    # print(xiny("A", IUB_CODES["C"]))
-# 
-
-
-function which_name(tf)
-    fieldnames(tf)[find(tf)]
-end
-
 function subcodes(code)
-    return IUB_CODES[code]
-    # which_name(mapslices(code -> in(IUB_CODES).(code)))
+    codes = ""
+
+    for (key, value) in IUB_CODES
+        if (issubset(value, IUB_CODES[code]))
+            codes *= key
+        end
+    end
+    
+    return str_to_vector(codes)
 end
 
-print(subcodes("K"))
-
-function complement(strings)
+function complement(oligo)
+    codes = ""
+    oligo_bases = str_to_vector(oligo)
     base_comps = Dict(
         "A" => "T", 
         "C" => "G", 
         "G" => "C", 
         "T" => "A",
     )
+
+    for code in oligo_bases
+        temp = ""
+        for base in IUB_CODES[code]
+            temp *= base_comps[base]
+        end
+
+        for (key, value) in IUB_CODES
+            if (issetequal(str_to_vector(temp), value))
+                codes *= key
+            end
+        end
+    end
+
+    return str_to_vector(codes)
 end
 
-#= IUB_CODES Description
-    Dictionary consisting of keys and values (IUB codes)
-
-    Keys:
-        Input to call a value of the Dictionary
-    Values:
-        Output of the IUB codes used by CutFree
-=#
-
-# IUB_CODES Testing
-    # println(IUB_CODES["R"][2])
-# 
-
-
-
 # Overall time test
-    #=
-    @time begin
-        println(IUB_CODES["R"][2])
-        println(get_blocking_codes("A", true))
-        println(str_to_vector("CGTACCCGG"))
-        print_oligo_block("AY")
-    end
-    =#
+    # @time begin
+    #     println(IUB_CODES["R"][2])
+    #     println(get_blocking_codes("A", true))
+    #     println(str_to_vector("CGTACCCGG"))
+    #     print_oligo_block("AY")
+    # end
 #
---
+++ CutFree.md
@@ -0,0 +1,101 @@
+# IUB_Codes
+
+## Description:
+Dictionary consisting of IUB keys and associated nucleotide values.
+
+## Examples:
+```julia-repl
+julia> println(IUB_CODES["R"][2])
+G
+```
+
+# get_blocking_codes(code, names)
+
+## Description:
+Find IUB codes that do not contain a specified nucleotide code.
+
+If 'names' is true, a vector of the possible IUB codes is returned. 
+
+If 'names' is false, a vector of the possible nucleotide groups is returned.
+
+## Examples:
+```julia-repl
+julia> print(get_blocking_codes("A", true))
+["T", "C", "G", "Y", "B", "S", "K"]
+julia> print(get_blocking_codes("A", false))
+[["T"], ["C"], ["G"], ["C", "T"], ["C", "G", "T"], ["C", "G"], ["G", "T"]]
+```
+
+# str_to_vector(str)
+
+## Description:
+Transform a string into a vector of single characters.
+
+## Examples:
+```julia-repl
+julia> println(str_to_vector("CGTACCCGG"))
+SubString{String}["C", "G", "T", "A", "C", "C", "C", "G", "G"]
+```
+
+# make_oligo_block(oligo)
+
+## Description:
+Creates a text block showing which bases are allowed by the oligo. 
+
+Returns four strings denoting the presence of each base.
+
+## Examples:
+```julia-repl
+julia> print(make_oligo_block("ACGC"))
+["A---", "----", "-C-C", "--G-"]
+```
+
+# print_oligo_block(oligo)
+
+## Description:
+Prints the oligo and the output of make_oligo_block.
+
+## Examples:
+```julia-repl
+julia> print_oligo_block("NAGN")
+NAGN
+AA-A
+T--T
+C--C
+G-GG
+```
+
+# xiny(x,y)
+
+## Description:
+Returns a boolean value determining if x is contained within a vector y
+
+## Examples:
+```julia-repl
+julia> print(xiny("A", IUB_CODES["N"]))
+true
+julia> print(xiny("A", IUB_CODES["C"]))
+false
+```
+
+# subcodes(code)
+
+## Description:
+Return array of IUB codes that are subsets of provided IUB code.
+
+## Examples:
+```julia-repl
+julia> print(subcodes("H"))
+SubString{String}["A", "W", "T", "C", "Y", "M", "H"]
+```
+
+# compliment(oligo)
+
+## Description:
+Return array the complements for any given IUB codes.
+
+## Examples:
+```julia-repl
+julia> print(complement("HA"))
+SubString{String}["D", "T"]
+```
