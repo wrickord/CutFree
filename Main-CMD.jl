a1ff18829331db6c618bd3079c04fa333bfb4296
Walker Rickord <wrickord27@gmail.com>
2023-01-24 13:50:46 -0600

    CutFree updated to allow for different sized restriction sites. Virtual environment created to reduce package issues.

---
+++ Main-CMD.jl
@@ -0,0 +1,24 @@
+import Pkg
+
+"""
+main()
+
+    Description:
+    Run simulations using your command line.
+"""
+function CutFreeCMD(starting_oligo::String, restriction_sites::Vector{String}, min_blocks::Int64, increase_diversity::Bool)
+    Pkg.activate("venv")
+
+    include("CutFree.jl")
+    include("CutFreeRL.jl")
+
+    CutFree_out = @timed cutfree(starting_oligo, restriction_sites, min_blocks, increase_diversity)
+    print("Time: ", CutFree_out.time, " seconds")
+end
+
+starting_oligo = "NNNNNNNNNNNNNNNNNNNN"
+restriction_sites = ["GGTCTC", "GGCCGG"]
+min_blocks = 1
+increase_diversity = true
+
+CutFreeCMD(starting_oligo, restriction_sites, min_blocks, increase_diversity)
