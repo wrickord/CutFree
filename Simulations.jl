e0a2dc7c9dc5bc1bb7cb5259c236aec0453ee066
Walker Rickord <walkerrickord@gmail.com>
2023-02-13 14:09:30 -0600

    Updated simulation functions to write data to CSV.

--- Simulations.jl
+++ Simulations.jl
@@ -1,3 +1,13 @@
+import Pkg
+
+Pkg.activate("cutfree-venv")
+Pkg.instantiate()
+
+using CSV, DataFrames
+
+include("CutFree.jl")
+include("CutFreeRL.jl")
+
 """
 read_restriction_sites(file)
 
@@ -6,14 +16,14 @@ read_restriction_sites(file)
     Adjust results based on length and palindromic arguments.
 """
 function read_restriction_sites(file="rebase_data.csv", enzyme_length=6, is_palindromic=true)
-    df = DataFrame(CSV.File(file))
+    df = CSV.read(file, DataFrame)
     commercial_df = df[df[:,"commercial_source"].!="NA",:] # clean to only include commercially available restriction enzymes
     temp_df = commercial_df[commercial_df[:,"length"].==enzyme_length,:] # clean based on length specification
     re_df = temp_df[temp_df[:,"is_palindrome"].==is_palindromic,:] # clean based on palendromic specification
 
     blocking_sites = []
     for seq in re_df[:,"base_sequence"]
-        push!(blocking_sites, seq)
+        push!(blocking_sites, String(seq))
     end
     return blocking_sites
 end
@@ -35,6 +45,45 @@ function get_oligos(oligo_min_length=6, oligo_max_length=20)
 end
 
 """
+write_output(oligo, sites)
+
+    Description:
+    Record all simulation results and output to CSV file.
+"""
+function write_output(oligo, sites)
+    cutfree_output = @timed cutfree(oligo, sites, 1, true)
+    cutfreeRL_output = @timed cutfreeRL(oligo, sites, simulate=simulate_random, nsims=1000)
+
+    file = "cutfree-simulations.csv"
+    output = DataFrame(
+        Oligo = oligo,
+        OligoLength = length(oligo),
+        Sites = sites,
+        TotalSites = length(sites),
+        SiteLength = length(sites[1]),
+        CutFree_Randomer = String(cutfree_output.value),
+        CutFree_Degeneracy = degeneracy(String(cutfree_output.value)),
+        CutFree_Time = cutfreeRL_output.time,
+        CutFreeRL_Randomer = String(cutfreeRL_output.value),
+        CutFreeRL_Degeneracy = degeneracy(String(cutfreeRL_output.value)),
+        CutFreeRL_Time = cutfreeRL_output.time,
+    )
+
+    CSV.write(file, output, append=true)
+end
+
+"""
+read_input(oligo, sites)
+
+    Description:
+    Read all simulation data from CSV.
+"""
+function read_input(file="cutfree-simulations.csv")
+    df = CSV.read(file, DataFrame)
+    return df
+end
+
+"""
 main()
 
     Description:
@@ -42,9 +91,10 @@ main()
 """
 function main()
     blocking_sites = read_restriction_sites("rebase_data.csv", 6, true)
-    oligos = get_oligos(6, 20)
-
-    print(oligos)
+    oligos = get_oligos(6, 10)
+    for oligo in oligos
+        write_output(oligo, [blocking_sites[1]])
+    end
 end
 
 main()
