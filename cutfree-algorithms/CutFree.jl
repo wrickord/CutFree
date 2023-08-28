0cebcea31e70203f681a595fe49769cf1b7a1311
Walker Rickord <walkerrickord@gmail.com>
2023-08-28 18:03:39 -0500

    Updated CutFree code, tweaked data organization, updated BERT model, added increased randomization of data via shuffling original set and tensors

--- cutfree-algorithms/CutFree.jl
+++ cutfree-algorithms/CutFree.jl
@@ -1,5 +1,7 @@
+# import packages
 using Clp, DataStructures, GLPK, Gurobi, JuMP, NamedArrays
 
+# define IUB code dictionary
 IUB_CODES = Dict(
     "A" => ["A"], 
     "C" => ["C"], 
@@ -18,36 +20,38 @@ IUB_CODES = Dict(
     "N" => ["A", "C", "G", "T"]
 )
 
+
+# helper functions
 """
-get_blocking_codes(code, names)
+blocking_codes(code, show_codes)
 
     Description:
     Find IUB codes that do not contain a specified nucleotide code.
-    If 'names' is true, a vector of the possible IUB codes is returned. 
-    If 'names' is false, a vector of the possible nucleotide groups is returned.
+    If 'show_codes' is true:
+        - A vector of the possible IUB codes is returned
+    If 'show_codes' is false: 
+        - A vector of the possible nucleotides is returned
 
     Examples:
-    julia> println(get_blocking_codes("A", true))
+    julia> println(blocking_codes("A", true))
 
-    Any["T", "C", "G", "Y", "B", "S", "K"]
-    julia> print(get_blocking_codes("A", false))
+    "T", "C", "G", "Y", "B", "S", "K"
 
-    Any[["T"], ["C"], ["G"], ["C", "T"], ["C", "G", "T"], ["C", "G"], ["G", "T"]]
+    julia> print(blocking_codes("A", false))
+
+    ["T"], ["C"], ["G"], ["C", "T"], ["C", "G", "T"], ["C", "G"], ["G", "T"]
 """
-function get_blocking_codes(code, names)
+function blocking_codes(code, show_codes)
     blocking_codes = []
     for (key, value) in IUB_CODES
-        if findall(x -> x in value, IUB_CODES[code]) == Int64[] && !names
-            push!(blocking_codes, value)
-        end
-
-        if findall(x -> x in value, IUB_CODES[code]) == Int64[] && names
-            push!(blocking_codes, key)
+        if findall(x -> x in value, IUB_CODES[code]) == Int64[]
+            push!(blocking_codes, show_codes ? key : value)
         end
     end
 
     return blocking_codes
 end
+
 
 """
 str_to_vector(str)
@@ -64,6 +68,7 @@ function str_to_vector(str)
     return string.(collect(str))
 end
 
+
 """
 vector_to_str(vctr)
 
@@ -79,13 +84,13 @@ function vector_to_str(vctr)
     return join(vctr)
 end
 
+
 """
 make_oligo_block(oligo)
 
     Description:
-    Creates a text block showing which bases are allowed by the oligo. 
-
-    Returns four strings denoting the presence of each base.
+    Creates a text block showing which bases are allowed by the oligo and
+    returns four strings denoting the presence of each base at each position.
 
     Example:
     julia> println(make_oligo_block("ACGC"))
@@ -98,7 +103,7 @@ function make_oligo_block(oligo)
     i = 0
     for code in str_to_vector(oligo)
         for base in IUB_CODES[code]
-            for (key, value) in strings
+            for (key, _) in strings
                 if (key == base)
                     strings[key] *= key
                 end
@@ -116,6 +121,7 @@ function make_oligo_block(oligo)
     return collect(values(strings))
 end
 
+
 """
 print_oligo_block(oligo)
 
@@ -132,9 +138,13 @@ print_oligo_block(oligo)
     G-GG
 """
 function print_oligo_block(oligo)
-    println(oligo * "\n" * make_oligo_block(oligo)[1] * "\n" * make_oligo_block(oligo)[2]
-    * "\n" * make_oligo_block(oligo)[3] * "\n" * make_oligo_block(oligo)[4])
+    println(
+        oligo * "\n" * make_oligo_block(oligo)[1] * "\n" 
+        * make_oligo_block(oligo)[2] * "\n" * make_oligo_block(oligo)[3] 
+        * "\n" * make_oligo_block(oligo)[4]
+    )
 end
+
 
 """
 subcodes(code)
@@ -148,7 +158,6 @@ subcodes(code)
     ["A", "W", "T", "C", "Y", "M", "H"]
 """
 function subcodes(code)
-    
     codes = ""
     for (key, value) in IUB_CODES
         if (issubset(value, IUB_CODES[code]))
@@ -158,6 +167,7 @@ function subcodes(code)
     
     return str_to_vector(codes)
 end
+
 
 """
 complement(oligo)
@@ -175,10 +185,7 @@ function complement(oligo)
 
     codes = ""
     for code in str_to_vector(oligo)
-        temp = ""
-        for base in IUB_CODES[code]
-            temp *= base_comps[base]
-        end
+        temp = join([base_comps[base] for base in IUB_CODES[code]])
 
         for (key, value) in IUB_CODES
             if (issetequal(str_to_vector(temp), value))
@@ -190,20 +197,22 @@ function complement(oligo)
     return str_to_vector(codes)
 end
 
+
 """
-get_reverse_complement(oligo)
+reverse_complement(oligo)
 
     Description:
     Return the reverse of an array of the complements for any given IUB codes.
 
     Example:
-    julia> println(get_reverse_complement("HDN"))
+    julia> println(reverse_complement("HDN"))
 
     ["N", "H", "D"]
 """
-function get_reverse_complement(oligo)
+function reverse_complement(oligo)
     return reverse(complement(oligo))
 end
+
 
 """
 expand_asymmetric(oligo)
@@ -217,8 +226,9 @@ expand_asymmetric(oligo)
     ["HDN", "NHD"]
 """
 function expand_asymmetric(oligo)
-    return [oligo, vector_to_str(get_reverse_complement(oligo))]
+    return [oligo, vector_to_str(reverse_complement(oligo))]
 end
+
 
 """
 get_degeneracy(oligo)
@@ -232,26 +242,30 @@ get_degeneracy(oligo)
     4.1588830833596715
 """
 function get_degeneracy(oligo)
-    value = 0
-    for code in str_to_vector(oligo)
-        if code == "-"
-            value += 0
-        else
-            value += log(length(IUB_CODES[code]))
-        end
-    end
+    value = sum(
+        code == "-" ? 0 : log(length(IUB_CODES[code])) 
+        for code in str_to_vector(oligo)
+    )
 
     return value
 end
+
 
 """
 cutfree()
 
     Description:
-    Return a site-free random of maximum diversity (of nucleotides). This single degenerate sequence can be used to synthesize a pool of oligos with mixed bases.
+    Return a site-free random of maximum diversity (of nucleotides). 
+    This single degenerate sequence can be used to synthesize a pool of 
+    oligos with mixed bases.
 
     Example:
-    julia> cutfree()
+    julia> cutfree(
+        "NNNNNNNNNNNNNNNNNNNN", 
+        ["GGTCTC", "GGCCGG"], 
+        1, 
+        true
+    )
 
     NNNNNWDNHDNHDNHDNDNN
     AAAAAAAAAAAAAAAAAAAA
@@ -261,7 +275,12 @@ cutfree()
     24.731283462223622
     "NNNNNWDNHDNHDNHDNDNN"
 
-    julia> cutfree(starting_oligo = "GNGNNNYBDKVDNGCTNNNNN", restriction_sites = ["GGTCTC", "GGCCGG"], min_blocks = 1, increase_diversity = true)
+    julia> cutfree(
+        "GNGNNNYBDKVDNGCTNNNNN", 
+        ["GGTCTC", "GGCCGG"], 
+        1, 
+        true
+    )
 
     GNGNDNYKWKVDNGCTNNNNN
     -A-AAA--A-AAA---AAAAA
@@ -272,13 +291,18 @@ cutfree()
     "GNGNDNYKWKVDNGCTNNNNN"
 """
 function cutfree(
-            starting_oligo::String,
-            restriction_sites::Any,
-            min_blocks = 1,
-            increase_diversity = false
-            )
+    starting_oligo::String,
+    restriction_sites::Any,
+    min_blocks = 1,
+    increase_diversity = false
+)
     
-    starting_oligo = join(map(x -> isspace(starting_oligo[x]) ? "" : starting_oligo[x], 1:length(starting_oligo)))
+    starting_oligo = join(
+        map(
+            x -> isspace(starting_oligo[x]) ? "" : starting_oligo[x], 
+            1:length(starting_oligo)
+        )
+    )
 
     m = length(starting_oligo)
 
@@ -288,8 +312,12 @@ function cutfree(
         push!(sites, expand_asymmetric(restriction_sites[i])[2])
     end
 
-    A = NamedArray(zeros(15, m), (["A", "C", "G", "T", "R", "Y", "M", "K", "S", "W", "H", "B", "V", "D", "N"],
-        1:m), ("IUB_CODES", "Position"))
+    A = NamedArray(
+        zeros(15, m), 
+        (["A", "C", "G", "T", "R", "Y", "M", "K", "S", "W", "H", "B", "V", 
+        "D", "N"], 1:m), 
+        ("IUB_CODES", "Position")
+    )
     for i in 1:m
         subs = subcodes(string(starting_oligo[i]))
         for b in subs
@@ -299,11 +327,16 @@ function cutfree(
 
     B = []
     for rs in sites
-        B_i = NamedArray(zeros(15, length(rs)), (["A_blocked", "C_blocked", "G_blocked", "T_blocked", "R_blocked", "Y_blocked",
-            "M_blocked", "K_blocked", "S_blocked", "W_blocked", "H_blocked", "B_blocked", "V_blocked", 
-            "D_blocked", "N_blocked"], 1:length(rs)), ("IUB_CODES", "Position"))
+        B_i = NamedArray(
+            zeros(15, length(rs)), 
+            (["A_blocked", "C_blocked", "G_blocked", "T_blocked", "R_blocked", 
+            "Y_blocked", "M_blocked", "K_blocked", "S_blocked", "W_blocked", 
+            "H_blocked", "B_blocked", "V_blocked", "D_blocked", "N_blocked"], 
+            1:length(rs)), 
+            ("IUB_CODES", "Position")
+        )
         for j in 1:length(rs)
-            blocked = get_blocking_codes(string(rs[j]), true)
+            blocked = blocking_codes(string(rs[j]), true)
             for b in blocked
                 B_i[string(b, "_blocked"), j] = 1
             end
@@ -312,38 +345,64 @@ function cutfree(
         push!(B, B_i)
     end
     
-    model = Model(Gurobi.Optimizer) # Gurobi can be changed to any open source solver you wish to use
-    set_time_limit_sec(model, 60.0)
-    set_silent(model)
+    model = Model(Gurobi.Optimizer) # can be changed to any open source solver
+    set_time_limit_sec(model, 60.0) # optional time limit
+    set_silent(model) # optional silence output
 
-    C = zeros(15, m) # Degeneracy matrix
+    C = zeros(15, m) # degeneracy matrix
     for i in 1:15, j in 1:m
-        C[i, j] = log(length(IUB_CODES[names(A,1)[i]])) # Assign degeneracy values to matrix
+        C[i, j] = log(length(IUB_CODES[names(A, 1)[i]])) # assign degeneracy
     end
 
-    @variable(model, output[1:15, 1:m], Bin) # Create output variable
-    @constraint(model, sum(output[j, i] for i in 1:m, j in findall(x->x==0, A[:, i])) .== 0) # Enforce selected codes exist within subcodes(starting_oligo)
-    @constraint(model, sum(output[i, 1:m] for i=1:15) .<= 1) # Enforce one code selected for each position
+    # create output variable
+    @variable(
+        model, 
+        output[1:15, 1:m], Bin
+    ) 
+
+    # enforce selected codes exist within subcodes(starting_oligo)
+    @constraint(
+        model, 
+        sum(
+            output[j, i] for i in 1:m, j in findall(x->x==0, A[:, i])
+        ) .== 0
+    ) 
+
+    # enforce one code selected for each position
+    @constraint(
+        model, 
+        sum(
+            output[i, 1:m] for i=1:15
+        ) .<= 1
+    ) 
     
     for rs in sites, j in 1:m-length(rs)+1
-        @constraint(model, sum(output[:, j:j+length(rs)-1] .* B[findall(x->x==rs, sites)[1]]) .>= min_blocks) # Enforce presence of blocking codes
+        # enforce presence of blocking codes
+        @constraint(
+            model, 
+            sum(
+                output[:, j:j+length(rs)-1] .* B[findall(x->x==rs, sites)[1]]
+            ) .>= min_blocks
+        ) 
     end
     
-    @objective(model, Max, sum(C .* output)) # Maximize degeneracy   
+    @objective(model, Max, sum(C .* output)) # maximize degeneracy   
     optimize!(model)
 
-    # Increase diversity of solution
+    # increase diversity of solution
     if (increase_diversity == true)
-        # Run CutFree with a constraint addeed requiring the solution to be equal to the optimal objective value
+        # run cutfree with a constraint addeed requiring the solution to be 
+        # equal to the optimal objective value
         @constraint(model, sum(C .* output) == objective_value(model))
 
-        # Change objective function to the sum of all variables multiplied by random coefficients (randomize)
+        # change objective function to the sum of all variables multiplied 
+        # by random coefficients (randomize)
         D = rand(15, m)
         @objective(model, Max, sum(D .* output))
         optimize!(model)
     end
 
-    # Find oligo from output matrix, return empty if no possible output
+    # find oligo from output matrix, return empty if no possible output
     oligo = ""
     for i in 1:m, j in 1:15
         try value.(output[j, i])
